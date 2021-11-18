a566c21bc239342cbca769f957cac6d6b3f2f6ca
James Wu <ichangjwu@outlook.com>
2021-11-19 01:27:47 +0900

    edited code and README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# bubble-sort
+# Bubble Sort
+## About
+This is the 4th mini-project from the Basic Ruby section of the TOP curriculum. The task is to take an array and return it sorted without using #sort. It must use the bubble sort methodology.
+## Skills Practiced
+- Nested loops
+- Assigning multiple variables in one line
--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,14 +2,13 @@ def bubble_sort(array)
   2.upto(array.length) do |j|
     for i in 0..array.length-j
       if array[i] > array[i+1]
-        temp = array[i]
-        array[i] = array[i+1]
-        array[i+1] = temp
+        array[i], array[i+1] = array[i+1], array[i]
       end
     end
   end
   array
 end
-  
+
+# Test
 ary = [89, 26, 32, 93, 13, 33, 51, 82, 32, 77]
-p bubble_sort(ary)
+p bubble_sort(ary) # => [13, 26, 32, 32, 33, 51, 77, 82, 89, 93]
